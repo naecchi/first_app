5fed68d71197044530805200fa8a144efba336b4
naecchi <71973149+naecchi@users.noreply.github.com>
2025-04-28 18:07:35 +0900

    ’posts#index’のビュー作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,4 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all  # すべてのレコードを@postsに代入
   end
-
-  def new
-  end
-  
-  def create
-    Post.create(content: [:content])
-    redirect_to "/posts"
-  end
-
 end
